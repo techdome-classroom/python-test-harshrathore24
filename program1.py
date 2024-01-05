b77f92d72cc1037973a83e37c3aafd239a23934a
harshrathore24 <rathoreharsh304@gmail.com>
2024-01-05 13:28:01 +0530

    complited the solutions

--- program1.py
+++ program1.py
@@ -1,13 +1,30 @@
-class Solution(object):
-    def isValid(self, s):
-        """
-        :type s: str
-        :rtype: bool
-        """
-        pass
-    
+from queue import LifoQueue
+class Solution:
+    def isValid(self, s: str) -> bool:
+        n = len(s)
+        stack = LifoQueue()
+        for i in s:
+            if i in {'}',']',')'} and stack.empty() == True:
+                return False
+            if i in {'(', '{' , '['}:
+                stack.put(i)
+            if i in {')'}:
+                if stack.queue[-1] == '(':
+                    stack.get()
+                else:
+                    return False
+            if i in {']'}:
+                if stack.queue[-1] == '[':
+                    stack.get()
+                else:
+                    return False
+            if i in {'}'}:
+                if stack.queue[-1] == '{':
+                    stack.get()
+                else:
+                    return False
 
-
-
-  
-
+        if stack.empty() == True:
+            return True
+        else:
+            return False
--- program2.py
+++ program2.py
@@ -1,7 +1,24 @@
-class Solution(object):
-    def romanToInt(self, s):
-        """
-        :type s: str
-        :rtype: int
-        """
-        pass
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        translations = {
+            "I": 1,
+            "V": 5,
+            "X": 10,
+            "L": 50,
+            "C": 100,
+            "D": 500,
+            "M": 1000
+        }
+        number = 0
+        pre_value = 0
+
+        for char in s:
+            value = translations[char]
+            if value > pre_value:
+                number += value - 2 * pre_value
+            else:
+                number += value
+
+            pre_value = value
+
+        return number
